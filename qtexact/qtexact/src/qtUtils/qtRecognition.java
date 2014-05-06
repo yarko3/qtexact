d2a94a8d739b61346aff7742e8b96fd871814ef3
Yarko Senyuta <yarko3@hotmail.com>
2014-05-06 11:39:34 -0700

    worked on qtRecognitionYan

--- qtexact/qtexact/src/qtUtils/qtRecognition.java
+++ qtexact/qtexact/src/qtUtils/qtRecognition.java
@@ -1,6 +1,9 @@
 package qtUtils;
 
 import java.util.Collection;
+import java.util.Hashtable;
+import java.util.Iterator;
+import java.util.PriorityQueue;
 
 import edu.uci.ics.jung.graph.DelegateForest;
 import edu.uci.ics.jung.graph.Graph;
@@ -22,20 +25,48 @@ public class qtRecognition
 		
 		//array of vertices
 		Integer[] vertexArray = (Integer[]) vertices.toArray();
+		
+		//create a hashtable to store visited vertices
+		Hashtable<Integer, Boolean> visited = new Hashtable<Integer, Boolean>();
 		for (int j = 1; j <= vertexArray.length; j++)
 		{
 			if (G.getInEdges(vertexArray[j]).size() >= 1)
 			{
 				//find neighbours of vertexArray[j]
 				Collection<Integer> neighbours = G.getNeighbors(vertexArray[j]);
-				
-				
+				//create neighbour priority queue based on their in degree
+				PriorityQueue<vertexIn<Integer>> pQueue = new PriorityQueue<vertexIn<Integer>>();
+				for (int n : neighbours)
+				{
+					pQueue.add(new vertexIn<Integer>(n, G.inDegree(n)));
+				}
+				//choose a neighbour that fits criteria
+				Iterator<vertexIn<Integer>> iterator = pQueue.iterator();
+				while (iterator.hasNext())
+				{
+					vertexIn<Integer> next = iterator.next();
+					//add edge to F
+					if ((G.degree(next.getVertex()) < vertexArray[j]) || (G.degree(next.getVertex()) == G.degree(vertexArray[j]) && visited.contains(next.getVertex())))
+					{
+						F.addEdge("e:" +  next.getVertex() + "-" + vertexArray[j], next.getVertex(), vertexArray[j]);
+						break;
+					}
+				}
 			}
+			visited.put(vertexArray[j], true);
 		}
+		
+		//check if number of ancestors of each vertex == in degree
+		for (int j : vertices)
+		{
+			
+		}
+		
+		
 	}
 	
 	//class containing vertex and inDegree for PriorityQueue in qtCheckYan
-	private class vertexIn<V>
+	private class vertexIn<V> implements Comparable<vertexIn<V>>
 	{
 		private V vertex;
 		private int inDegree;
@@ -47,5 +78,9 @@ public class qtRecognition
 		
 		private V getVertex(){return vertex;};
 		private int getInDegree(){return inDegree;};
+		public int compareTo(vertexIn<V> v)
+		{
+			return Integer.compare(inDegree, v.getInDegree());
+		}
 	}
 }
