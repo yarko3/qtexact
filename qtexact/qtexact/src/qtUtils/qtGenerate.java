617b5cd6612b2cebccc4508d8d40aab292dca831
Yarko Senyuta <yarko3@hotmail.com>
2014-05-08 12:53:14 -0700

    working sort

--- qtexact/qtexact/src/qtUtils/qtGenerate.java
+++ qtexact/qtexact/src/qtUtils/qtGenerate.java
@@ -4,9 +4,20 @@ import java.util.Collection;
 
 import edu.uci.ics.jung.graph.Graph;
 import edu.uci.ics.jung.graph.SparseGraph;
-
+/**Class used for generating quasi-threshold graphs
+ * 
+ * @author Yarko Senyuta
+ *
+ * @param <V> vertex
+ * @param <E> edge
+ */
 public class qtGenerate<V, E>
 {
+	/**
+	 * generate a completely connected graph of number of edges
+	 * @param number
+	 * @return
+	 */
 	public static Graph<Integer, String> qtGraph(int number)
 	{
 		SparseGraph<Integer, String> graph = new SparseGraph<Integer, String>();
@@ -14,7 +25,7 @@ public class qtGenerate<V, E>
 		{
 			graph.addEdge("e:" + 1 + "-" + 2, 1, 2);
 			int curEdge = 3;
-			int level = 1;
+			int level = 2;
 			while (level <= number)
 			{
 				Integer[] array = new Integer[graph.getVertexCount()];
