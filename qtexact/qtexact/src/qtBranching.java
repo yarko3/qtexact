af5086c2ccc8c0ea43833917dee234c949da44b3
Yarko Senyuta <yarko3@hotmail.com>
2014-05-13 11:23:08 -0700

    more work on branching

--- qtexact/qtexact/src/qtBranching.java
+++ qtexact/qtexact/src/qtBranching.java
@@ -26,61 +26,129 @@ public class qtBranching
 		
 	}
 	
-	private Graph<Integer, String> branching(Graph<Integer, String> G, ArrayList<LinkedList<Integer>> deg, int changes)
+	private branchingReturnType branching(Graph<Integer, String> G, ArrayList<LinkedList<Integer>> deg, int changes)
 	{
 		//check if graph is QT
 		ArrayList<Integer> lexResult = genericLBFS.genericLexBFS(G);
 		//qt graph has been found
 		if (lexResult.size() == G.getVertexCount() && lexResult.get(4) != Character.getNumericValue('C') && lexResult.get(4) != Character.getNumericValue('P'))
 		{
-			return G;
+			return new branchingReturnType(G, deg, changes);
 		}
 		//branch on found P4 or C4
 		else
 		{	
-			//C4 has been found, add 2 edges to remove C4
+			//C4 has been found
 			if (lexResult.get(4) == Character.getNumericValue('C'))
 			{
-				//make copy of graph to change
-				Graph<Integer, String> copy = clone.deepClone(G);
+				//result of adding 2 edges to break C4
+				branchingReturnType c4Add = c4AddResult(G, deg, changes, lexResult);
 				
-				//update degree sequence (first edge)
-				deg.get(G.degree(lexResult.get(0))).remove(lexResult.get(0));
-				if (deg.get(G.degree(lexResult.get(0)) + 1) == null)
-				{
-					deg.add(G.degree(lexResult.get(0)) + 1, new LinkedList<Integer>());
-					deg.get(G.degree(lexResult.get(0)) + 1).add(lexResult.get(0));
-				}
-				deg.get(G.degree(lexResult.get(0))).remove(lexResult.get(2));
-				if (deg.get(G.degree(lexResult.get(2)) + 1) == null)
-				{
-					deg.add(G.degree(lexResult.get(2)) + 1, new LinkedList<Integer>());
-					deg.get(G.degree(lexResult.get(2)) + 1).add(lexResult.get(2));
-				}
-				
-				//update degree sequence (second edge)
-				deg.get(G.degree(lexResult.get(1))).remove(lexResult.get(0));
-				if (deg.get(G.degree(lexResult.get(1)) + 1) == null)
-				{
-					deg.add(G.degree(lexResult.get(1)) + 1, new LinkedList<Integer>());
-					deg.get(G.degree(lexResult.get(1)) + 1).add(lexResult.get(1));
-				}
-				deg.get(G.degree(lexResult.get(3))).remove(lexResult.get(3));
-				if (deg.get(G.degree(lexResult.get(3)) + 1) == null)
-				{
-					deg.add(G.degree(lexResult.get(3)) + 1, new LinkedList<Integer>());
-					deg.get(G.degree(lexResult.get(3)) + 1).add(lexResult.get(3));
-				}
-				
-				//add edges
-				copy.addEdge("e:" + lexResult.get(0) + "-" + lexResult.get(2), lexResult.get(0), lexResult.get(2));
-				copy.addEdge("e:" + lexResult.get(1) + "-" + lexResult.get(3), lexResult.get(1), lexResult.get(3));
-				
+				//results of removing 2 edges to break C4
+				branchingReturnType c4Remove0 = c4DeleteResult(G, deg, changes, lexResult.get(0), lexResult.get(1), lexResult.get(1), lexResult.get(2));
+				branchingReturnType c4Remove1 = c4DeleteResult(G, deg, changes, lexResult.get(0), lexResult.get(3), lexResult.get(2), lexResult.get(3));
+				branchingReturnType c4Remove2 = c4DeleteResult(G, deg, changes, lexResult.get(0), lexResult.get(1), lexResult.get(2), lexResult.get(3));
+				branchingReturnType c4Remove3 = c4DeleteResult(G, deg, changes, lexResult.get(0), lexResult.get(3), lexResult.get(1), lexResult.get(2));
+				branchingReturnType c4Remove4 = c4DeleteResult(G, deg, changes, lexResult.get(1), lexResult.get(2), lexResult.get(2), lexResult.get(3));
+				branchingReturnType c4Remove5 = c4DeleteResult(G, deg, changes, lexResult.get(0), lexResult.get(1), lexResult.get(0), lexResult.get(3));
 				
 				
 			}
+			//P4 has been found
+			else
+			{
+				
+			}
+				
 			
 		}
+	}
+	
+	
+	//result of adding 2 edges to break C4
+	private branchingReturnType c4AddResult(Graph<Integer, String> G, ArrayList<LinkedList<Integer>> deg, int changes, ArrayList<Integer> lexResult)
+	{
+		//make copy of graph and degree sequence
+		Graph<Integer, String> graphCopy = clone.deepClone(G);
+		ArrayList<LinkedList<Integer>> degCopy = clone.deepClone(deg);
+		
+		//update degree sequence (first edge)
+		addEdge(graphCopy, degCopy, lexResult.get(0), lexResult.get(2));
+		
+		//update degree sequence (second edge)
+		addEdge(graphCopy, degCopy, lexResult.get(1), lexResult.get(3));
+		
+		
+		return branching(graphCopy, degCopy, changes + 2);
+		
+	}
+	
+	private branchingReturnType c4DeleteResult(Graph<Integer, String> G, ArrayList<LinkedList<Integer>> deg, int changes, Integer v0, Integer v1, Integer v2, Integer v3)
+	{
+		//make copy of graph and degree sequence
+		Graph<Integer, String> graphCopy = clone.deepClone(G);
+		ArrayList<LinkedList<Integer>> degCopy = clone.deepClone(deg);
+		
+		//update degree sequence (first edge)
+		removeEdge(graphCopy, degCopy, v0, v1);
+		
+		//update degree sequence (second edge)
+		removeEdge(graphCopy, degCopy, v2, v3);
+		
+		
+		return branching(graphCopy, degCopy, changes + 2);
+		
+	}
+	
+	
+	private static void removeEdge(Graph<Integer, String> G, ArrayList<LinkedList<Integer>> deg, Integer v0, Integer v1)
+	{
+		int v0Deg = G.degree(v0);
+		int v1Deg = G.degree(v1);
+		
+		deg.get(v0Deg).remove(v0);
+		if (deg.get(v0Deg - 1) == null)
+		{
+			deg.add(v0Deg - 1, new LinkedList<Integer>());
+		}
+		deg.get(v0Deg - 1).add(v0);
+		
+		deg.get(v1Deg).remove(v1);
+		if (deg.get(v1Deg - 1) == null)
+		{
+			deg.add(v1Deg - 1, new LinkedList<Integer>());
+		}
+		deg.get(v1Deg - 1).add(v1);
+		
+		//find the edge to remove
+		if (!G.removeEdge("e:" + v0 + "-" + v1))
+			G.removeEdge("e:" + v1 + "-" + v0);
+		
+		
+	}
+	private static void addEdge(Graph<Integer, String> G, ArrayList<LinkedList<Integer>> deg, Integer v0, Integer v1)
+	{
+		int v0Deg = G.degree(v0);
+		int v1Deg = G.degree(v1);
+		
+		deg.get(v0Deg).remove(v0);
+		if (deg.get(v0Deg + 1) == null)
+		{
+			deg.add(v0Deg + 1, new LinkedList<Integer>());
+		}
+		deg.get(v0Deg + 1).add(v0);
+		
+		deg.get(v1Deg).remove(v1);
+		if (deg.get(v1Deg + 1) == null)
+		{
+			deg.add(v1Deg + 1, new LinkedList<Integer>());
+		}
+		deg.get(v1Deg + 1).add(v1);
+		
+		//add edge
+		G.addEdge("e:" + v0 + "-" + v1, v0, v1);
+		
+		
 	}
 	
 	
@@ -89,10 +157,13 @@ public class qtBranching
 	{
 		Graph<Integer, String> G;
 		int changes;
-		branchingReturnType(Graph<Integer, String> graph, int c)
+		ArrayList<LinkedList<Integer>> deg;
+		
+		branchingReturnType(Graph<Integer, String> graph, ArrayList<LinkedList<Integer>> d, int c)
 		{
 			G = graph;
 			changes = c;
+			deg = d;
 		}
 	}
 	
