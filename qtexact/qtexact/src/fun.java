2ab6a6c5983392500dc204e2719cde39029bd13f
Yarko Senyuta <yarko3@hotmail.com>
2014-05-27 13:53:15 -0700

    er is breaking

--- qtexact/qtexact/src/fun.java
+++ qtexact/qtexact/src/fun.java
@@ -57,7 +57,8 @@ public class fun extends JApplet {
 		
 		//exampleQT = qtGenerate.nonQTEx3();
 		
-		exampleQT = gen.
+		exampleQT = gen.ER(10, 0.2);
+		
 //		exampleQT = new SparseGraph<Integer, Pair<Integer>>();
 //		fillGraphFromFile(exampleQT, "datasets/karate.txt");
 		
