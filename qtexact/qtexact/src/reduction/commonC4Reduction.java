b2337bdd215632801812ba1536ec30551bb0acd6
15857113 <15857113@CS112>
2014-06-17 11:30:00 -0700

    tried to parallelize edge reduction, did not work. Optimized c4 reduction

--- qtexact/qtexact/src/reduction/commonC4Reduction.java
+++ qtexact/qtexact/src/reduction/commonC4Reduction.java
@@ -42,14 +42,10 @@ public class commonC4Reduction<V> extends Reduction<V>
 		//look through every two non-neighbours
 		ArrayList<V> vertices = new ArrayList<V>();
 		vertices.addAll(s.getG().getVertices());
-		
-		
 		outer:
-		for (int i = 0; i < vertices.size(); i++)
+		for (V v0 : s.getG().getVertices())
 		{
-			V v0 = vertices.get(i);
 			
-			//if this node does not have enough neighbours to satisfy reduction, skip
 			if (s.getG().getNeighborCount(v0) < s.getMinMoves().getChanges().size() - s.getChanges().size())
 				continue;
 			
@@ -57,108 +53,56 @@ public class commonC4Reduction<V> extends Reduction<V>
 			{
 				for (V v1 : s.getG().getNeighbors(n0))
 				{
-					if (!s.getG().isNeighbor(v0, v1) && v0 != v1)
+					if (v1 != v0)
 					{
-						//get all common neighbours
-						HashSet<V> all = new HashSet<V>();
-						ArrayList<V> common = new ArrayList<V>();
-						
-						all.addAll(s.getG().getNeighbors(v0));
-						
-						//get all the duplicates in common, while all neighbours are stored in all
-						for (V v : s.getG().getNeighbors(v1))
+						if (!s.getG().isNeighbor(v0, v1))
 						{
-							if (!all.add(v))
-								common.add(v);
-						}
-						
-						
-						for (int k = 0; k < common.size(); k++)
-						{
-							V temp = common.remove(k);
-							boolean flag = true;
-							for (V n : common)
+							//get all common neighbours
+							HashSet<V> all = new HashSet<V>();
+							ArrayList<V> common = new ArrayList<V>();
+							
+							all.addAll(s.getG().getNeighbors(v0));
+							
+							//get all the duplicates in common, while all neighbours are stored in all
+							for (V v : s.getG().getNeighbors(v1))
 							{
-								if (!s.getG().isNeighbor(temp, n))
-									flag = false;
+								if (!all.add(v))
+									common.add(v);
 							}
 							
-							if (flag == false)
+							
+							for (int k = 0; k < common.size(); k++)
 							{
-								common.add(k, temp);
+								V temp = common.remove(k);
+								boolean flag = true;
+								for (V n : common)
+								{
+									if (!s.getG().isNeighbor(temp, n))
+										flag = false;
+								}
+								
+								if (flag == false)
+								{
+									common.add(k, temp);
+								}
+								else
+									k--;
 							}
-							else
-								k--;
-						}
-						
-						
-						//if number of induced C4s is greater than the allowed number of moves, add an edge
-						if (common.size() > s.getMinMoves().getChanges().size() - s.getChanges().size() && !toApply.contains(new myEdge<V>(new Pair<V>(v0, v1), true)))
-						{
 							
-							toApply.addLast(new myEdge<V>(new Pair<V>(v0, v1), true));
 							
-							if (toApply.size() == s.getMinMoves().getChanges().size() - s.getChanges().size())
-								break outer;
+							//if number of induced C4s is greater than the allowed number of moves, add an edge
+							if (common.size() > s.getMinMoves().getChanges().size() - s.getChanges().size() && !toApply.contains(new myEdge<V>(new Pair<V>(v0, v1), true)))
+							{
+								
+								toApply.addLast(new myEdge<V>(new Pair<V>(v0, v1), true));
+								
+								if (toApply.size() == s.getMinMoves().getChanges().size() - s.getChanges().size())
+									break outer;
+							}
 						}
 					}
 				}
-				
-				
 			}
-			
-			
-			
-//			for (int j = i+1; j < vertices.size(); j++)
-//			{
-//				V v1 = vertices.get(j);
-//				
-//				if (!s.getG().isNeighbor(v0, v1))
-//				{
-//					//get all common neighbours
-//					HashSet<V> all = new HashSet<V>();
-//					ArrayList<V> common = new ArrayList<V>();
-//					
-//					all.addAll(s.getG().getNeighbors(v0));
-//					
-//					//get all the duplicates in common, while all neighbours are stored in all
-//					for (V v : s.getG().getNeighbors(v1))
-//					{
-//						if (!all.add(v))
-//							common.add(v);
-//					}
-//					
-//					
-//					for (int k = 0; k < common.size(); k++)
-//					{
-//						V temp = common.remove(k);
-//						boolean flag = true;
-//						for (V n : common)
-//						{
-//							if (!s.getG().isNeighbor(temp, n))
-//								flag = false;
-//						}
-//						
-//						if (flag == false)
-//						{
-//							common.add(k, temp);
-//						}
-//						else
-//							k--;
-//					}
-//					
-//					
-//					//if number of induced C4s is greater than the allowed number of moves, add an edge
-//					if (common.size() > s.getMinMoves().getChanges().size() - s.getChanges().size() && !toApply.contains(new myEdge<V>(new Pair<V>(v0, v1), true)))
-//					{
-//						
-//						toApply.addLast(new myEdge<V>(new Pair<V>(v0, v1), true));
-//						
-//						if (toApply.size() == s.getMinMoves().getChanges().size() - s.getChanges().size())
-//							break outer;
-//					}
-//				}
-//			}
 		}
 		
 		stack.push(toApply.size());
@@ -179,33 +123,7 @@ public class commonC4Reduction<V> extends Reduction<V>
 		return s;
 	}
 	
-	/** 
-	 * Calculates n choose k (using dynamic programming)
-	 *   @param n the total number to choose from (n > 0)
-	 *   @param k the number to choose (0 <= k <= n)
-	 *   @return n choose k (the binomial coefficient)
-	 */
-	 private static int binomial(int n, int k) 
-	 { 
-	    if (n < 2) {
-	        return 1;
-	    }
-	    else {
-	        int bin[][] = new int[n+1][n+1];   
-
-	        for (int r = 0; r <= n; r++) {      
-	            for (int c = 0; c <= r && c <= k; c++) {
-	                if (c == 0 || c == r) {
-	                    bin[r][c] = 1;        
-	                }
-	                else {                      
-	                    bin[r][c] = bin[r-1][c-1] + bin[r-1][c];
-	                }
-	            }
-	        }
-	        return bin[n][k];
-	    }
-	 }
+	
 
 }
 
